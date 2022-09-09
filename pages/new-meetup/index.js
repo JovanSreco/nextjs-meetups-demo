19f9a46db30da24ef68139eceaeeb736153a9593
Jovan Sreco <jovan.sreco@gmail.com>
2022-09-09 09:38:41 +0200

    Ready for deployment

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,37 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetupPage = () => {
+  const router = useRouter();
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  };
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a new Meetup</title>
+        <meta
+          name="description"
+          content="Add a new Meetup create amazing networking opurtunities"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
+};
+
+export default NewMeetupPage;
